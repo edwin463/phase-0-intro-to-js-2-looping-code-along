2738b463539591d8134c5721159dbe0ab7779f01
edwin karuri njoroge <edwin.njoroge1@student.moringaschool.com>
2024-09-27 00:22:41 -0700

    loop

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+
+
+
+
+// Function that returns an array of thank you messages
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+// Function that counts down from a given number to zero and logs each number
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
